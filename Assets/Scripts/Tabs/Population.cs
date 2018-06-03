92fa99b698c2e7b2403d215a8f10c46c15435115
ABRVXUS <abrvxus@outlook.com>
2018-06-03 12:07:59 -0700

    Added GUIText to panel
    Population script is able to write to text. needs to be able to update.

--- Assets/Scripts/Tabs/Population.cs
+++ Assets/Scripts/Tabs/Population.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Population : MonoBehaviour
 {
     float pop;
+    private string txt;
 
     void Start()
     {
         pop = Planet.population;
+        txt = string.Format("Population: {0}", pop);
     }
 	
     //method for finding how many people are going to die
@@ -34,5 +37,9 @@ public class Population : MonoBehaviour
 
         return birthCount;
     }
-    
+
+    public void GUI()
+    {
+        GameObject.Find("PanelText").GetComponent<Text>().text = txt;
+    }
 }
